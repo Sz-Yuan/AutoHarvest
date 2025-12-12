12fafa39c3589595f4ae3151c352ca57796e11ef
SzYuan <szy030904@qq.com>
2025-12-12 08:59:15 +0800

    支持1.21.11，修复甘蔗无法在含水方块上种植

--- src/main/java/kite/autoharvest/util/WaterProximityChecker.java
+++ src/main/java/kite/autoharvest/util/WaterProximityChecker.java
@@ -5,7 +5,6 @@ import net.minecraft.core.Direction;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.material.FluidState;
 
